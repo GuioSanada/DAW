20809e7780667684ef3e68f203eaec49c764f85f
GuioSanada <afernandez.campos@gmail.com>
2022-01-24 13:34:35 +0100

    24/01

---
+++ diseñoInterfaces/cajaMov/js/script.js
@@ -0,0 +1,10 @@
+function getPos(event){
+    console.log(event.clientY);
+    let caja = document.getElementById("cajita");
+
+    if(event.clientY <= 50){
+        caja.style.top = `0`;
+    }else{
+        caja.style.top = `calc(${event.clientY}px - 50px)`;
+    }
+}
